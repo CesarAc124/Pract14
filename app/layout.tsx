8b9c8779c4f229ff2d6a071c404c1c78b485e164
CesarAc124 <cesar.acosta@g-tech.io>
2025-08-08 10:59:33 -0600

    create, update & delete ESTUDIANTES

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,12 @@ import {
   SidebarProvider,
 } from "@/components/ui/sidebar"
 import { Footer } from "@/components/ui/layout/footer";
- 
- 
+import { ConvexClientProvider } from "./ConvexClientProvider";
+
 export const iframeHeight = "800px"
- 
+
 export const description = "A sidebar with a header and a search form."
- 
+
 export const metadata: Metadata = {
   title: "Mi Escuela",
   description: "WebApp para gestionar mi escuela",
@@ -21,11 +21,11 @@ export const metadata: Metadata = {
     icon: "/favicon.ico",
   },
 };
- 
+
 type RootLayoutProps = Readonly<{
   children: React.ReactNode;
 }>;
- 
+
 export default function RootLayout({ children }: RootLayoutProps): React.ReactElement {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -38,18 +38,21 @@ export default function RootLayout({ children }: RootLayoutProps): React.ReactEl
           disableTransitionOnChange
         >
           <div className="[--header-height:calc(--spacing(14))]">
-            <SidebarProvider className="flex flex-col">
-              <SiteHeader />
-              <div className="flex flex-1">
-                <AppSidebar />
-                <SidebarInset>
-                  <div className="flex flex-1 flex-col gap-4 p-4">
-                    {children}
-                  </div>
-                  <Footer />
-                </SidebarInset>
-              </div>
-            </SidebarProvider>
+            
+              <SidebarProvider className="flex flex-col">
+                <SiteHeader />
+                <div className="flex flex-1">
+                  <AppSidebar />
+                  <SidebarInset>
+                    <ConvexClientProvider>
+                    <div className="flex flex-1 flex-col gap-4 p-4">
+                      {children}
+                    </div>
+                    <Footer />
+                    </ConvexClientProvider>
+                  </SidebarInset>
+                </div>
+              </SidebarProvider>
           </div>
         </ThemeProvider>
       </body>
