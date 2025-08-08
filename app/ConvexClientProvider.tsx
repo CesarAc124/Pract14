2398be33b569ebcbf376f49aae71c608d861cb1a
CesarAc124 <cesar.acosta@g-tech.io>
2025-08-08 11:14:36 -0600

    fix: resolve module not found errors

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -4,16 +4,17 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
 
-const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
-if (!convexUrl) {
-  throw new Error("NEXT_PUBLIC_CONVEX_URL no está definida");
-}
-const convex = new ConvexReactClient(convexUrl);
+
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
+  const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
+  if (!convexUrl) {
+    throw new Error("NEXT_PUBLIC_CONVEX_URL no está definida");
+  }
+  const convex = new ConvexReactClient(convexUrl);
   return (
     <ConvexProvider client={convex}>
       {children}
     </ConvexProvider>
-);
+  );
 }
